2718d749764c63f1764942ab470ca4f09d2dce7b
Dann Stayskal <dann@stayskal.com>
2012-04-24 21:53:22 -0700

    Implementing forward convergence on bidirectional network

--- lib/associative_memory/network.rb
+++ lib/associative_memory/network.rb
@@ -1,3 +1,5 @@
+require 'matrix'
+
 module AssociativeMemory
   class Network
 
@@ -33,76 +35,16 @@ module AssociativeMemory
       end
     end
 
-      # $a becomes input
-      # $b becomes output
-      # $memory{X} becomes self.input
-      # $memory{Y} becomes self.output
-      # $aPrev becomes previous_input
-      # $aPrev becomes previous_output
+    def converge_input(input)
+      ### Multiply the input vector by the convergence matrix
+      input_vector = Matrix.row_vector(input)
+      convergence = Matrix.rows(self.matrix)
+      output_vector = input_vector * convergence
 
-    def converge(input, output)
-      previous_input = []
-      previous_output = []
-      ### If either vector changed on the last iteration, keep calculating
-      while (input != previous_input) || (output != previous_output) do
-        previous_input = input
-        previous_output = output
-
-        ### Step 6: Calculate new A->B iteration
-        output.each_with_index do |output_scalar, output_index|
-          weight = 0
-          input.each_with_index do |input_scalar, input_index|
-            weight += output_scalar * self.matrix[input_index][output_index]
-          end
-          if weight > 0
-            output[output_index] = 1
-          elsif weight < 0
-            output[output_index] = 0
-          end
-        end
-
-        # foreach my $j (0..scalar(int(@{$b}))-1){
-        #   my $sum = 0;
-        #   foreach my $i (0..scalar(int(@{$a}))-1){
-        #     $sum += $$b[$i] * $$memory{weights}[$i][$j];
-        #   }
-        #   if($sum > 0){
-        #     $$b[$j] = 1;
-        #   }elsif($sum < 0){
-        #     $$b[$j] = 0;
-        #   }
-        # }
-
-        ### Step 7: Calculate new B->A iteration
-        input.each_with_index do |input_scalar, input_index|
-          weight = 0
-          output.each_with_index do |output_scalar, output_index|
-            weight += input_scalar * self.matrix[input_index][output_index]
-          end
-          if weight > 0
-            input[input_index] = 1
-          elsif weight < 0
-            input[input_index] = 0
-          end
-        end
-
-        # foreach my $i (0..scalar(int(@{$a}))-1){
-        #   my $sum = 0;
-        #   foreach my $j (0..scalar(int(@{$b}))-1){
-        #     $sum += $$a[$i] * $$memory{weights}[$i][$j];
-        #   }
-        #   if($sum > 0){
-        #      $$a[$i] = 1;
-        #   }elsif($sum < 0){
-        #      $$a[$i] = 0;
-        #   }
-        # }
-
-        puts " input: #{input}   output: #{output}"
+      ### Threshold and return the resulting output vector
+      return output_vector.row(0).to_a.map do |element|
+        if element > 0 then 1 else 0 end
       end
-      self.input = input
-      self.output = output
-      return true
     end
 
   end 
