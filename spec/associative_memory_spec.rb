20dd21203ad79de20a909b5225cf7e10f85b0659
Dann Stayskal <dann@stayskal.com>
2012-04-24 23:58:57 -0700

    spaces -> tabs

--- spec/associative_memory_spec.rb
+++ spec/associative_memory_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe AssociativeMemory do
-  
-  it "should have a valid version number" do
-  	AssociativeMemory::VERSION.should match(/\d+\.\d+/)
-  end
-  
-  it "should pass method calls through to the network class" do
-  end
+	
+	it "should have a valid version number" do
+		AssociativeMemory::VERSION.should match(/\d+\.\d+/)
+	end
+	
+	it "should pass method calls through to the network class" do
+	end
 
 end
